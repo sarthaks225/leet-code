122abf53fafa061d88a467dbf6a89c8b9429141c
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2022-03-19 00:32:59 +0530

    Time: 18 ms (45.56%), Space: 10.4 MB (75.18%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -13,16 +13,18 @@ public:
     ListNode* oddEvenList(ListNode* head) {
         if(head==NULL) return NULL;
         if(head->next==NULL) return head;
-        ListNode *o_node,*e_node,*se;
+        ListNode *o_node,*e_node,*se,*dp;
         o_node=head;
         e_node=se=head->next;
-        while(e_node && e_node->next!=NULL)
+         dp=e_node->next;
+        while(e_node && dp)
         {
-            o_node->next=e_node->next; ;
-            o_node=e_node->next; ;
+
+            o_node->next=dp; ;
+            o_node=dp; ;
             e_node->next=o_node->next;
             e_node=o_node->next;
-            
+            if(e_node) dp=e_node->next;
         }
         o_node->next=se;
      
