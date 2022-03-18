f600e41fada42fabfd0950351b4a3bd8126f6d5a
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2022-03-19 00:25:54 +0530

    Time: 18 ms (45.56%), Space: 10.5 MB (30.86%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -16,14 +16,14 @@ public:
         ListNode *o_node,*e_node,*dp,*se;
         o_node=head;
         e_node=se=head->next;
-        dp=e_node->next;
-        while(e_node && dp)
+      dp=e_node->next;  
+        while(e_node && e_node->next!=NULL)
         {
-            o_node->next=dp;
-            o_node=dp;
-            e_node->next=dp->next;
-            e_node=dp->next;
-            if(e_node!=NULL) dp=e_node->next; 
+            o_node->next=e_node->next; ;
+            o_node=e_node->next; ;
+            e_node->next=o_node->next;
+            e_node=o_node->next;
+            
         }
         o_node->next=se;
      
