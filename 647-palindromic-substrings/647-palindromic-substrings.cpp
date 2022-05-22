b9e04a2a7bb52001df3515d043d552904c32f071
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2022-05-23 00:43:04 +0530

    Time: 161 ms (13.64%), Space: 6.1 MB (96.68%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int count=0;
-        
-        count=s.length();
-        
-        
-        string::iterator i1,i2,i3;
-        int flag;
-        for(int i=1; i<s.length(); ++i)
+        int count;
+        int length;
+        length=s.length();
+        count=length;
+        string::iterator i1,i2,i3,end;
+        end=s.end();
+        for(int i=1; i<length; ++i)
         {
             i1=s.begin();
-           
-           
-            
-            for( ;i1!=s.end()-i ;i1++)
+            for( ;i1!=end-i ;i1++)
             {
 
             i2=i1;
             i3=i2+i;
-                
-                
-            while(i2<i3)
-            {
-                if(*i2!=*i3)
-                {
+              while(i2<i3)
+              {
+                  if(*i2!=*i3) break;    
+                  i2++;
+                  i3--;
+              }
+            if(i2>i3 || i2==i3) count++;
                     
-                     break;    
-                }
-                
-                i2++;
-                i3--;
-            }
-                if(i2>i3 || i2==i3) 
-                {
-                    
-                    count++;
-                    
-                }
-                
-            }
+           }
             
         }
         
