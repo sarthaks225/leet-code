e6857fe234cd935d77267db6374d3f805a994d57
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-18 23:13:01 +0530

    Time: 3 ms (45.94%), Space: 7.2 MB (13.37%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
+        for(int i=0; i<matrix.size(); ++i) //for transpose of Matrix
+        {
+            for(int j=i+1; j<matrix.size(); ++j)
+            {
+                std::swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int i=0; i<matrix.size(); ++i)
+        {
+            for(int j=0; j<matrix.size()/2; ++j)
+            {
+                swap(matrix[i][j],matrix[i][matrix.size()-1-j]);
+            }
+        }
+        
+        
+        /*
         int size=matrix.size();
         vector<vector<int>> newMatrix;
         newMatrix.resize(size);
@@ -16,14 +34,11 @@ public:
 
         for(int i=0; i<size; ++i)
         {
-            
             for(int j=0; j<size; ++j)
             {
                 matrix[i][j]=newMatrix[i][j];
             }
         }
-
-
-
+        */
     }
 };
