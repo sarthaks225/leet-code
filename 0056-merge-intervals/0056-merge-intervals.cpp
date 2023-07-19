5c2bd533466e21f6cc753e53e594e1bb2ac5fc7d
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-19 19:01:05 +0530

    Time: 2402 ms (5.07%), Space: 19.9 MB (21.38%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        for(int i=0; i<intervals.size(); ++i)
+        {
+            
+            for(int j=i+1; j<intervals.size(); ++j)
+            {
+                if(intervals[i][0]!=-1 && intervals[j][0]!=-1)
+                {
+                    
+                    if(intervals[i][1]>=intervals[j][0] && intervals[i][0]<=intervals[j][0])
+                    {
+                        cout<<"****"<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
+                        cout<<intervals[j][0]<<" "<<intervals[j][1]<<endl;
+                        
+                        intervals[i][1]=intervals[i][1]>intervals[j][1]?intervals[i][1]:intervals[j][1];
+                        
+                        intervals[j][0]=-1;
+                        i-=1;
+                        break;
+                    }
+                    else if(intervals[i][0]>=intervals[j][0] && intervals[i][0]<=intervals[j][1])
+                    {
+                        cout<<"****"<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
+                        cout<<intervals[j][0]<<" "<<intervals[j][1]<<endl;
+                         intervals[i][0]=intervals[j][0];
+                         intervals[i][1]=intervals[i][1]>intervals[j][1]?intervals[i][1]:intervals[j][1];
+                        intervals[j][0]=-1;
+                        i-=1;
+                        break;
+                    }
+                }
+                    
+                }
+                
+            }
+        
+        vector<vector<int>> nInt;
+        vector<int> *vect;
+        for(int i=0,j=0; i<intervals.size(); ++i)
+        {
+            if(intervals[i][0]!=-1)
+            {
+                vect=new vector<int>;
+                vect->resize(2);
+                vect[j][0]=intervals[i][0];
+                vect[j][1]=intervals[i][1];
+                nInt.push_back(*vect);
+            }
+        }
+        
+        return nInt;
+    
+            
+        }
+        
+        
+};
