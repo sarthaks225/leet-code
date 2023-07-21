71d64d2d294b5a7aef83bb6be42c56ca22801761
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-21 21:27:58 +0530

    Time: 0 ms (100.00%), Space: 9 MB (39.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,13 +2,47 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
+        if(n==0) return;
         
+        vector<int>::iterator itr1,itr2,itr3;
+        itr1=nums1.end()-1;
+        itr2=nums1.begin()+(m-1);
+        itr3=nums2.end()-1;
+        while(itr3>=nums2.begin() && itr2>=nums1.begin())
+        {
+            cout<<" itr2:"<<*itr2<<"  itr3:"<<*itr3<<endl;
+            if(*itr2<=*itr3)
+            {
+                *itr1=*itr3;
+                --itr1;
+                --itr3;
+            }
+            else
+            {
+                *itr1=*itr2;
+                --itr1;
+                *itr2=0;
+                --itr2;
+            }
+            
+            
+        }
+        
+        if(itr3>=nums2.begin())
+        {
+            for(itr1=nums1.begin(),itr2=nums2.begin(); itr2<=itr3; ++itr1,++itr2 )
+            {
+                *itr1=*itr2;
+            }
+        }
+        /*
         // time comp O(n)+O((m+n)log(m+n))
         for(int i=m,j=0; j<n; ++j,++i)
         {
             nums1[i]=nums2[j];
         }
         sort(nums1.begin(),nums1.end());
+        */
         
         /*
             //*** time comp O(max(m,n)) + O(m+n)
