ab49b92d9953b2e495c96327152171016415d5b5
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-22 10:58:55 +0530

    Time: 744 ms (5.00%), Space: 61.1 MB (75.96%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
+        int i=nums[0];
+        int j=i; 
+        while(1)
+        {
+           
+             
+            i=nums[i];
+            j=nums[nums[j]];
+             cout<<nums[j]<<" "<<nums[i]<<" ,";
+            if(i==j) break;
+            
+        }
+        cout<<"sdf";
+        for(i=nums[0]; i!=j; i=nums[i])
+        {
+            cout<<nums[j]<<" "<<nums[i];
+            j=nums[j];
+        }
+        return j;
+        /*
+        
         vector<int> freq;
         freq.resize(nums.size());
 
@@ -12,6 +33,8 @@ public:
                 else freq[*itr]++;
             
         }
+        
         return 0;
+        */
     }
 };
