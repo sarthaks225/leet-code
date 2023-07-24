98b3523d7e4fd2aaeac60e5520c25fbaf0e199bd
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-24 12:19:05 +0530

    Time: 148 ms (81.68%), Space: 83.2 MB (54.24%) - LeetHub

--- 0775-global-and-local-inversions/0775-global-and-local-inversions.cpp
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool isIdealPermutation(vector<int>& nums) {
-        int k=nums.size()-1;
-      
-           for(int j=2,i=0; i<nums.size(); ++i )
-           {
-               if(abs(nums[i]-i)>1) return false;
-           }
-    
-       
-      
-        return true;
+           for(int i=0; i<nums.size(); ++i ) if(abs(nums[i]-i)>1) return false;
+           return true;
         
     }
 };
