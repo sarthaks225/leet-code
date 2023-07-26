4ceb15e96e028df9feb6e505b062277471d1a4e4
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-26 23:15:59 +0530

    Time: 16 ms (55.17%), Space: 15.8 MB (63.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
+        map<int,int> mp;
+        
+        vector<int>::iterator i1;
+        vector<int> result;
+        int req=nums.size()/3;
+        int freq;
+        for(i1=nums.begin(); i1<nums.end(); ++i1)
+        {
+            freq=++mp[*i1];
+            if( freq>req && ( result.size()==0 || *i1!=result[0] ) )
+            {
+                result.push_back(*i1);
+                if(result.size()==2) break;
+            }
+        }
+        
+        return result;
+        
+        
+        
+        /*
+                // ********* time comp O(n^2) spc complexity O(1)
         vector<int>::iterator i1,i2;
         vector<int> result;
         int req=nums.size()/3;
@@ -31,6 +53,6 @@ public:
         
         return result;
         
-        
+        */      
     }
 };
