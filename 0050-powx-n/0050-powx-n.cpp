dc80ed0e776ae3fa2535faf0c5239e7c9d198c4b
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-26 12:21:34 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (59.96%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==1 || n==0) return 1;
+        double q=1;
+        double p=x;
+        long nn=n>0?n:(long)-1*n;
+        while(nn>1)
+        {
+            
+            if(nn%2==0)
+            {
+                p*=p;
+                nn/=2;
+                
+            }else
+            {
+                q*=p;
+                nn-=1;
+            }
+        }
+        double ans=p*q;
+        cout<<ans;
+        if(n<0) ans=1/ans;
+        return ans;
+    }
+};
