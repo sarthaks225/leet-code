f30958011596878bda79e12989e16571316b971f
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-26 15:35:47 +0530

    Time: 26 ms (33.36%), Space: 19.5 MB (51.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,6 +2,30 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
        
+        vector<int>::iterator i1;
+        int assumedMajority;
+        int count=0;
+        for(i1=nums.begin(); i1<nums.end(); ++i1)
+        {
+            if(count==0) 
+            {
+                assumedMajority=*i1;
+                ++count;
+            }
+            else if(assumedMajority!=*i1) --count;
+            else ++count;
+        }
+        
+        for(i1=nums.begin(),count=0; i1<nums.end(); ++i1)
+        {
+            if(assumedMajority==*i1) ++count;
+        }
+        
+        if(count>nums.size()/2) return assumedMajority;
+        return -1;
+        
+        /*
+            //****** TIME COMP worst case O(N) space comp worst case O(N)
         unordered_map<int,int> mp;
         vector<int>::iterator i1;
         int ep=nums.size()/2;
@@ -13,6 +37,7 @@ public:
         }
         
         return -1;
+        */
         
         /*
             // time comp O(n^2) **********
