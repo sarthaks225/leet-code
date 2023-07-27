c491d9d30ddf14d1cffdf06795146aa60f61c7bf
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-07-27 17:04:37 +0530

    Time: 3 ms (38.27%), Space: 6 MB (85.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     
+    int comb(int n,int r)
+    {
+        long result=1;
+      
+        for(int i=1; i<=r ; ++i)
+        {
+            result=result*(n-r+i)/i;
+           // cout<<result<<"  ";
+        }
+        
+       
+        return result;
+    }
+    int uniquePaths(int m, int n) {
+        cout<<m+n-2<<"  "<<m-1<<endl;
+        return comb(m+n-2,m-1);
+    }
+    
+  /*
+        // time comp O(mXn) spc comp O(mXn) ***********
     int rec(int i,int j, int m, int n,vector<vector<int>>&dp)
     {
         if(i>m || j>n) return 0;
@@ -21,6 +41,7 @@ public:
                 }
         }
         return rec(0,0,m-1,n-1,dp);
-        
+      
     }
+    */
 };
