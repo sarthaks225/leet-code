335999eb9032432e3fd3d669b5618b7ad384036e
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-08-16 21:38:04 +0530

    Time: 3 ms (88.00%), Space: 10.9 MB (28.14%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void recur(set<vector<int>> &resultSet, vector<int> &v,int &target, vector<int>::iterator itr,vector<int> &candidates)
+    {
+       for(vector<int>::iterator i=itr; i!=candidates.end(); ++i)
+       {
+           if(itr!=i && itr!=candidates.begin() && *(itr-1)==*i) continue;
+           if(*i>target) return;
+           target-=*i;
+           v.push_back(*i);
+           if(target==0)
+           {
+               resultSet.insert(v);
+               v.pop_back();
+           target+=*i; 
+               return;
+           }
+           recur(resultSet,v,target, i+1,candidates);
+           v.pop_back();
+           target+=*i;    
+           
+           
+       }
+        
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        sort(candidates.begin(),candidates.end());
+        set<vector<int>> resultSet;
+        vector<int> v;
+        
+        recur(resultSet,v,target, candidates.begin(),candidates);
+        
+              vector<vector<int>> result(resultSet.begin(), resultSet.end());
+        return result;
+        
+    }
+};
