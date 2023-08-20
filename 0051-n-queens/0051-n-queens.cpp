50be3e34a477c964e5b3e96a8e6b6622a386377e
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-08-20 19:52:23 +0530

    Time: 3 ms (89.12%), Space: 7.2 MB (71.69%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -2,15 +2,46 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         
-        
         vector<vector<string>> result;
         string s(n,'.');
         vector<string> board(n,s);
-
-        recur(0,board,result,n);
+        vector<int> diagonalUp(2*n-1,0);
+        vector<int> diagonalDown(2*n-1,0);
+        vector<int> left(n,0);
+        recur(0,board,result,n, left, diagonalUp, diagonalDown);
         return result;
     }
+
+    void recur(int col,vector<string> &board, vector<vector<string>> &result, int n, vector<int> &left, vector<int> &diagonalUp, vector<int> &diagonalDown)
+    {
+        
+        if(col==n)
+        {
+            result.push_back(board);
+            return;
+        }
+        
+        for(int row=0; row<n; ++row)
+        {
+            if(left[row]==0 && diagonalUp[n-1+row-col]==0 && diagonalDown[row+col]==0)
+            {
+                board[row][col]='Q';
+                
+                left[row]=1;
+                diagonalUp[n-1+row-col]=1;
+                diagonalDown[row+col]=1;
+                recur(col+1, board, result, n, left, diagonalUp, diagonalDown);
+                
+                board[row][col]='.';
+                
+                left[row]=0;
+                diagonalUp[n-1+row-col]=0;
+                diagonalDown[row+col]=0;
+            }
+        }
+    }
     
+    /*
     void recur(int col,vector<string> &board, vector<vector<string>> &result, int n)
     {
         
@@ -54,5 +85,5 @@ public:
         return true;
         
     }
-    
+  */  
 };
