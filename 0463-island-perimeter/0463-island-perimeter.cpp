883aeee6ad2888668df9cec0017b62d2028d9840
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-09-02 19:01:33 +0530

    Time: 103 ms (53.19%), Space: 97.1 MB (29.29%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -6,18 +6,18 @@ public:
         {
             for(j=0; j<grid[0].size(); ++j)
             {
-                if(grid[i][j]==1) break;
+                if(grid[i][j]==1)
+                {
+                    vector<int> x={-1,0,0,1};
+                    vector<int> y={0,-1,1,0};
+                    grid[i][j]=2;
+                    return recur(i,j,x,y , grid);
+                }
             }
-            if(j<grid[0].size() && grid[i][j]==1) break;
         }
-        vector<int> x={-1,0,0,1};
-        vector<int> y={0,-1,1,0};
-        set<pair<int,int>> visited;
-        visited.insert({i,j});
-        //cout<<"**";
-        return recur(i,j,x,y , grid, visited);
+       return 0;
     }
-    int recur(int i,int j,vector<int> &x, vector<int> &y, vector<vector<int>> &grid, set<pair<int,int>> &visited)
+    int recur(int i,int j,vector<int> &x, vector<int> &y, vector<vector<int>> &grid)
     {
         int result=0;
         for(int n=0; n<4; ++n)
@@ -26,15 +26,12 @@ public:
             int col=j+y[n];
             if( (row>=0 && col>=0) && (row<grid.size() && col<grid[0].size()) )
             {
-                if(visited.count({row,col})==0)
-                {
                     if(grid[row][col]==1) 
                     {
-                        visited.insert({row,col});
-                        result+=recur(row,col, x,y, grid, visited);
+                        grid[row][col]=2;
+                        result+=recur(row,col, x,y, grid);
                     }
-                    else result+=1;
-                }
+                    else if(grid[row][col]==0) result+=1;
             }
             else  result+=1;
             
