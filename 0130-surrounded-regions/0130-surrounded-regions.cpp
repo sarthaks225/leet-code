658703256a9e4f3b958c734c081d1ddfe8723c6b
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-09-04 22:14:09 +0530

    Time: 11 ms (68.00%), Space: 10.2 MB (64.26%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        vector<vector<char>> check(board.size(), vector<char> (board[0].size(),0));
+        //cout<<"1111"<<endl;
+        for(int j=0; j<board[0].size(); ++j)
+        {
+                     //   cout<<j<<" "<<board[0][j];
+            if(board[0][j]=='X' || check[0][j]=='1') continue;
+            //cout<<j;
+            dfs(0,j,check,board);
+        }
+   // cout<<"22222";
+        for(int i=0; i<board.size(); ++i)
+        {
+            if(board[i][0]=='X' || check[i][0]=='1') continue;
+            dfs(i,0,check,board);
+        }
+        for(int i=board.size()-1,j=0; j<board[0].size(); ++j)
+        {
+            if(board[i][j]=='X' || check[i][j]=='1') continue;
+            dfs(i,j,check,board);
+        }
+        
+        for(int i=0,j=board[0].size()-1; i<board.size(); ++i)
+        {
+            if(board[i][j]=='X' || check[i][j]=='1') continue;
+            dfs(i,j,check,board);
+        }
+        
+        for(int i=0; i<board.size(); ++i)
+        {
+            for(int j=0; j<board[0].size(); ++j)
+            {
+                if(check[i][j]=='1') continue;
+                board[i][j]='X';
+            }
+        }
+                                   
+        
+    }
+    
+    void dfs(int i,int j, vector<vector<char>> &check, vector<vector<char>> &board)
+    {
+      if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]=='X' || check[i][j]=='1') return;
+        check[i][j]='1';
+        dfs(i-1,j, check, board);
+        dfs(i,j-1, check, board);
+        dfs(i,j+1, check, board);
+        dfs(i+1,j, check, board);
+        
+    }
+    
+};
