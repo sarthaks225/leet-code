9a25c90fdf33e1a6d689e08dd24f006556a0052f
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-11-11 18:33:51 +0530

    Time: 28 ms (22.97%), Space: 23.6 MB (35.05%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,7 +14,7 @@ public:
     {
         if(root==NULL || root==p || root==q) return root;
         if((root->val < p->val && root->val > q->val) || (root->val > p->val && root->val < q -> val) )return root;
-        if(root->val > p->val && root->val > q->val) return recur(root->left,p,q);
+        if(root->val > p->val) return recur(root->left,p,q); //root->val > p->val && root->val > q->val
         return recur(root->right,p,q);
     }
     
