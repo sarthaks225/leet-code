c57bd3423da12b5b781730eb3c33d2faf3ad3c41
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-11-13 12:26:54 +0530

    Time: 225 ms (7.90%), Space: 6.2 MB (94.21%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,9 +1,39 @@
 class Solution {
 public:
+    void generateNext(string &num)
+    {
+       string::reverse_iterator it1,it2;
+        it2=num.rbegin();
+        for(it1=it2+1; it1<num.rend(); it1++,it2++)
+        {
+            if(*it1<*it2) break;
+        }
+        if(it1==num.rend())
+        {
+            reverse(num.begin(),num.end());
+            return;
+        }
+        
+        for(it2=num.rbegin(); it2<it1; ++it2)
+        {
+            if(*it1<*it2)
+            {
+                swap(*it1,*it2);
+                break;
+            }
+        }
+        
+        reverse(num.rbegin(),it1);
+        
+    }
+    
     string getPermutation(int n, int k) {
-        string str;
-        for(int i=1; i<=n; ++i) str+=to_string(i);
-        while(--k) next_permutation(str.begin(),str.end());
-        return str;
+        string num="";
+        for(int i=1; i<=n; ++i) num+=to_string(i);
+        while(--k)
+        {
+            generateNext(num);
+        }
+        return num;
     }
 };
