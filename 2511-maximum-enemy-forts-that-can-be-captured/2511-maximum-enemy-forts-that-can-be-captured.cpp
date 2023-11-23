75f7697908fe48b2ff8befc208d7270fa7152daf
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2023-11-23 23:51:23 +0530

    Time: 2 ms (46.85%), Space: 8.1 MB (48.03%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
@@ -7,7 +7,7 @@ public:
         int i=0;
         for(i=0; i<nums.size(); ++i)
         {
-            if(nums[i]==1 || nums[i]==-1)
+            if(nums[i]!=0)
             {
                 loca=nums[i];
                 indexLoca=i;
