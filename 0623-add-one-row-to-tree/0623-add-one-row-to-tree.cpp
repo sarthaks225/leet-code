de4d3c03529a72864066f0680f39748d98a82bfe
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-04-16 23:32:46 +0530

    Time: 16 ms (32.77%), Space: 24.2 MB (74.48%) - LeetHub

---
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void add(TreeNode *node, int val)
+    {
+        TreeNode *leftNode=new TreeNode(val, node->left, NULL);
+        node->left=leftNode;
+        TreeNode *rightNode=new TreeNode(val, NULL, node->right);
+        node->right=rightNode;
+        
+    }
+    void recur(TreeNode *node, int val, int depth, int currDepth){
+        if(!node) return;
+        ++currDepth;
+        if(depth == currDepth){
+            add(node,val);
+            return;
+        }
+        recur(node->left,val,depth,currDepth);
+        recur(node->right,val,depth,currDepth);
+    }
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth==1){
+            TreeNode *leftNode=new TreeNode(val, root, NULL);
+            leftNode->left=root;
+            root=leftNode;
+            
+           
+            
+        }
+        else recur(root,val,depth,1);
+        return root;
+        
+    }
+};
