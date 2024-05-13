2982871d498a7ff692ba434efefff21543dc86bb
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-05-13 22:15:24 +0530

    Time: 12 ms (85.03%), Space: 17.9 MB (10.77%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,6 +11,49 @@
 class Solution {
 public:
     
+                //     TIME COMP (k*n)log(k)
+                //     SPACE COMP k*n
+    class Comparator{
+        public: 
+        bool operator()(const pair<int,ListNode *> &a, const pair<int, ListNode *> &b){
+            return a.first > b.first;
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        priority_queue<pair<int,ListNode *>,vector<pair<int,ListNode *>>, Comparator> pq;
+        
+        for(auto list : lists ){
+            if(list) pq.push( {list->val, list} );
+        }
+        
+        ListNode *start=new ListNode();
+        ListNode *node,*prev,*curr;
+        prev=start;
+        
+        while(!pq.empty()){
+            
+            curr=pq.top().second;
+            
+            node=new ListNode();
+            node->val=curr->val;
+            prev->next=node;
+            prev=node;
+            
+            pq.pop();
+            if(curr->next) pq.push( {curr->next->val, curr->next} );
+            
+        }
+        
+        return start->next;
+    }
+    
+    
+    
+    /*
+        // TIME COMP    (k*n)log(k*n) + log(k*n)
+        //space comp k*n
     class Comparator{
         public:
         bool operator()(int a,int b){
@@ -46,4 +89,5 @@ public:
         
         return start->next;
     }
+    */
 };
