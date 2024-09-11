e368bcdd114a44e92656fe37095bd52895d4b229
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-09-11 23:28:23 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (58.88%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -3,7 +3,7 @@ public:
     char findTheDifference(string s, string t) {
         
         char result = 0;
-        for(int i=0; i<s.length() && i<t.length(); ++i )
+        for(int i=0; i<s.length(); ++i )
         {
             result ^=( s[i] ^ t[i] );
         }
