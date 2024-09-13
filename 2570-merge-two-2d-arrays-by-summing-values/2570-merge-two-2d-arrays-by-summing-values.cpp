0e9d547df3caf84230cac007e4e98e5732f7ee76
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-09-14 01:35:18 +0530

    Time: 12 ms (16.41%), Space: 13.8 MB (78.95%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -2,6 +2,43 @@ class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
         
+        vector<vector<int>> result;
+        vector<vector<int>> :: iterator i1, i2;
+        
+        for( i1=nums1.begin(), i2=nums2.begin(); i1!=nums1.end() && i2!=nums2.end(); )
+        {
+            if((*i1)[0] == (*i2)[0])
+            {
+                result.push_back({(*i1)[0], (*i1)[1] + (*i2)[1]});
+                ++i1;
+                ++i2;
+            }else
+            {
+                if((*i1)[0] < (*i2)[0])
+                {
+                    result.push_back({(*i1)[0], (*i1)[1]});
+                   ++i1;
+                   
+                }
+                else
+                {
+                    result.push_back({(*i2)[0], (*i2)[1]});
+                 ++i2;
+                }
+            }
+            
+        }
+        
+        for( ; i1!=nums1.end(); ++i1)
+        {
+            result.push_back({(*i1)[0], (*i1)[1]});
+        }
+        for( ; i2!=nums2.end(); ++i2)
+        {
+            result.push_back({(*i2)[0], (*i2)[1]});
+        }
+        return result;
+        /*
         map<int , int> mp;
         
         for( auto nums : nums1)
@@ -25,5 +62,6 @@ public:
            result.push_back( {item.first, item.second} ); 
         }
         return result;      
+        */
     }
 };
