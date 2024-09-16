b394a57d503941eec01316df2a7b07f1f90cf321
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-09-16 18:47:42 +0530

    Time: 19 ms (11.99%), Space: 17.3 MB (98.24%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        sort(timePoints.begin(), timePoints.end());
+        int result=INT_MAX;
+        int bound2 = 24*60;
+        int min1=0;
+        int min2=0;
+        
+        // for(auto t : timePoints)
+        // {
+        //     cout<<t << endl;
+        // }
+        for(int i=1; i<timePoints.size(); ++i)
+        {
+            if( timePoints[i-1] == timePoints[i] )
+            {
+                cout<<"***"<<endl;
+                return 0;
+            }
+            else
+            {
+                
+                min1 = stoi( timePoints[i-1].substr(0, timePoints[i-1].find(':'))) * 60;
+                min2 = stoi( timePoints[i].substr(0,  timePoints[i].find(':'))) * 60;
+                
+                min1 += stoi ( timePoints[i-1].substr(timePoints[i-1].find(':')+1));
+                min2 += stoi ( timePoints[i].substr(timePoints[i].find(':')+1));
+                
+                if(min1 == 0 )
+                {
+                    result = min( result, min(abs(bound2 - min2), min2) );
+                }
+                else
+                {
+                    result = min( result, abs(min1 - min2));
+                }
+                cout << min1 << " "<<min2<<endl;
+            }
+        }
+        
+        
+         min1 = stoi( timePoints[0].substr(0, timePoints[0].find(':'))) * 60;
+        
+          min2 = stoi( timePoints[timePoints.size()-1].substr(0,  timePoints[timePoints.size()-1].find(':'))) * 60;
+          min1 += stoi ( timePoints[0].substr(timePoints[0].find(':')+1));
+        min2 += stoi ( timePoints[timePoints.size()-1].substr(timePoints[timePoints.size()-1].find(':')+1));
+        
+        result = min( result, min ( abs(min1 - min2), abs( (bound2 + min1 - min2)) ) );
+        return result;
+    }
+};
