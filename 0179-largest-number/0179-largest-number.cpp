0c8868fec0be8f4bf30a48c4686df436983cd908
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-09-19 10:50:09 +0530

    Time: 9 ms (34.31%), Space: 15.6 MB (85.37%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -5,17 +5,19 @@ public:
         if(a == b) return false;
         string s1 = to_string(a);
         string s2 = to_string(b);
-        int i=0;
-        while(true)
-        {
+                 return (s1+s2) > (s2+s1);
 
-            if( s1[i] > s2[i]) return true;
-            else if(s1[i] < s2[i]) return false;
-            ++i;
-            if( i == s1.size() && i == s2.size()) return true;
+        int i=0;
+//         while(true)
+//         {
+
+//             if( s1[i] > s2[i]) return true;
+//             else if(s1[i] < s2[i]) return false;
+//             ++i;
+//             if( i == s1.size() && i == s2.size()) return true;
             
-            if( i == s1.size() || i == s2.size()) return s1+s2 > s2+s1;
-        }
+//             if( i == s1.size() || i == s2.size()) return s1+s2 > s2+s1;
+//         }
     }
     string largestNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end(), comparator);
