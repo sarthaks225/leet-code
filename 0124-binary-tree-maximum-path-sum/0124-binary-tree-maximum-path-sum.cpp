cfe8bd1111855c4bcbca039240113d035f2d2a37
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-10-11 21:51:46 +0530

    Time: 20 ms (41.25%), Space: 27.9 MB (7.79%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -10,21 +10,22 @@
  * };
  */
 class Solution {
-public:
-    int recur(TreeNode *node, int &result)
+    int recur(TreeNode * root, int &result)
     {
-        int left=INT_MIN;
-        int right=INT_MIN;
-        if(node->left) left=recur(node->left,result);
-        if(node->right) right=recur(node->right,result);
-        int curr=node->val;
-        if((left>=0 || node->val>=0) && left+node->val>node->val) curr+=left;
-        if((right>=0 || node->val>=0) && right+node->val>node->val) curr+=right;
-        if(result<curr) result=curr;
-        return left>0?(right>0?(left>right?left+node->val:right+node->val):left+node->val):(right>0?right+node->val:node->val);
+        if(root == NULL ) return 0;
+        
+        int leftSum = recur(root->left, result);
+        int rightSum = recur(root->right, result);
+        result = max(result, root->val);
+        result = max(result, root->val +  leftSum);
+        result = max(result, root->val +  rightSum);
+        result = max(result, root->val +  leftSum + rightSum);
+        return max( root->val, root->val + max(leftSum, rightSum) );
     }
+    
+public:
     int maxPathSum(TreeNode* root) {
-        int result=INT_MIN;
+        int result = root->val;
         recur(root,result);
         return result;
     }
