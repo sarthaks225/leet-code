d4671b6062320adea07250a1d689add23b0991b9
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2024-10-12 11:30:43 +0530

    Time: 0 ms (100.00%), Space: 13.4 MB (55.94%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -9,79 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include<deque>
 class Solution {
 public:
-   
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
-        if(root==NULL) return {};
+        if(root == NULL) return {};
+        bool flag = true;
+        queue<TreeNode *> q;
         vector<vector<int>> result;
-        /*
-        deque<TreeNode *> q;
-        q.push_front(root);
-        int l;
-        bool flag=true;
-        while(!q.empty())
-        {
-            l=q.size();
-            vector<int> ans;
-            if(flag)
-            {
-                while(l--)
-                {
-                    ans.push_back(q.back()->val);   
-                    if(q.back()->left) q.push_front(q.back()->left);
-                    if(q.back()->right) q.push_front(q.back()->right);
-                    q.pop_back();
-                }
-               
-            }else
-            {
-                while(l--)
-                {
-                    ans.push_back(q.front()->val);
-                    if(q.front()->right) q.push_back(q.front()->right);
-                    if(q.front()->left) q.push_back(q.front()->left);
-                    q.pop_front();
-                }                
-            }
-            flag=!flag;
-            result.push_back(ans);
-        }
-        */
-        queue <TreeNode *> q;
-        bool flag=true;
         q.push(root);
-        int qSize;
+        
         while(!q.empty())
         {
-            qSize=q.size();
-            vector<int> ans(qSize);
+            int n=q.size();
+            vector<int> part;
             if(flag)
             {
-                while(qSize--)
+                while( n--)
                 {
+                    part.push_back(q.front()->val);
                     if(q.front()->left) q.push(q.front()->left);
                     if(q.front()->right) q.push(q.front()->right);
-                    ans[ans.size()-qSize-1]=q.front()->val;
-                    q.pop();
-                }
-                
-            }else
-            {
-                while(qSize--)
-                {
-                    if(q.front()->left) q.push(q.front()->left);
-                    if(q.front()->right) q.push(q.front()->right);
-                    ans[qSize]=q.front()->val;
                     q.pop();
                 }
             }
-            result.push_back(ans);
+            else
+            {
+                part.resize(n);
+                while(n--)
+                {
+                    part[n]=q.front()->val;
+                    if(q.front()->left) q.push(q.front()->left);
+                    if(q.front()->right) q.push(q.front()->right);
+                    q.pop();
+                }
+            }
+            result.push_back(part);
             flag=!flag;
         }
         
         return result;
+        
+        
+        
     }
 };
